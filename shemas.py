404261bbe2a009dd8f5a7e049fd269c7868a43ba
stepa <stepa.korobov@gmail.com>
2025-02-14 10:56:04 +0400

    remove shema Date

--- shemas.py
+++ shemas.py
@@ -1,22 +1,6 @@
-from datetime import datetime
 from typing import List
 
-from pydantic import BaseModel, Field, field_validator
-
-
-class Date(BaseModel):
-    """Валидатор даты"""
-
-    start_date: datetime = Field(default="2025-01-01 00:00:00")
-    end_date: datetime = Field(default="2025-01-01 00:00:00")
-
-    @field_validator("start_date", mode="before")
-    def start_data_validator(cls, v):
-        return datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
-
-    @field_validator("end_date", mode="before")
-    def end_data_validator(cls, v):
-        return datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
+from pydantic import BaseModel
 
 
 class Camera(BaseModel):
